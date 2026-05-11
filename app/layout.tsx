20542287317d8addcc9fc8b45c369764abad20c7
Abnet Mekonen Gosaye <abnetmekonen32@gmail.com>
2026-05-11 23:56:38 +0300

    fix: format description in metadata for improved readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AppProviders } from "./providers";
-import { fa } from "zod/locales";
 import icon from "public/repair-shop.png";
 
 export const metadata: Metadata = {
   title: "አቤ ጋራዥ ",
-  description: "Abe Garage is a car maintenance and repair service provider based in Ethiopia. We offer a wide range of services including oil changes, brake repairs, engine diagnostics, and more. Our team of experienced mechanics is dedicated to providing high-quality service to our customers.",
+  description:
+    "Abe Garage is a car maintenance and repair service provider based in Ethiopia. We offer a wide range of services including oil changes, brake repairs, engine diagnostics, and more. Our team of experienced mechanics is dedicated to providing high-quality service to our customers.",
   icons: {
     icon: icon.src,
   },
 };
-
 
 export default function RootLayout({
   children,
